b45f4636305b9217b5df12aadc6ac1a4b41f4682
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2011-09-27 18:12:02 +0200

    ajout d'un formulaire de login obligatoire si l'utilisateur n'a pas de session

--- index.php
+++ index.php
@@ -5,9 +5,33 @@ require_once ('lib/stc.php');
 
 
 stc_top();
-stc_menu_init();
-stc_menu_add('','');
-stc_menu();
+
+// menu
+$menu = stc_menu_init();
+
+if (stc_is_logged()) {
+  stc_menu_add_section ($menu, 'Propositions de Thèses');
+  stc_menu_add_item($menu, 'rechercher', 'test-1.php');
+  stc_menu_add_item($menu, 'proposer', 'test-2.php');
+  stc_menu_add_separator($menu);
+  stc_menu_add_section ($menu, 'Propositions de Stages');
+  stc_menu_add_item($menu, 'rechercher', 'test-1.php');
+  stc_menu_add_item($menu, 'proposer', 'test-2.php');
+} else {
+  stc_menu_add_form($menu,"post", "login.php");
+  stc_menu_form_add_text($menu,"Utilisateur","user");
+  stc_menu_form_add_password($menu,"Mot de Passe","password");
+  stc_menu_form_add_button($menu,"se connecter");
+  stc_menu_form_end($menu);
+  stc_menu_add_item($menu, "s'enregistrer", "register.php");
+}
+stc_menu($menu);
+
+// contenu
+?>
+blah blah blah
+<?php
+
 stc_footer();
 
 
--- lib/stc.php
+++ lib/stc.php
@@ -2,6 +2,12 @@
 
 require_once('xhtml.php');
 
+/******************************************************************************
+ *
+ * Fonctions html (entete, menu, ...)
+ * 
+ */
+
 function stc_top() {
   xhtml_header();
   ?><head>
@@ -13,18 +19,96 @@ function stc_top() {
 <?php
 }
 
+define ('STC_MENU_SECTION', 0);
+define ('STC_MENU_ITEM', 1);
+define ('STC_MENU_SEPARATOR', 2);
+define ('STC_MENU_FORM', 10);
+define ('STC_MENU_FORM_TEXT', 11);
+define ('STC_MENU_FORM_PASS', 12);
+define ('STC_MENU_FORM_BUTTON', 13);
+define ('STC_MENU_FORM_END', 14);
+
 function stc_menu_init () {
+  $menu = array();
+  return $menu;
 }
 
-function stc_menu_add () {
+function stc_menu_add_section (&$menu, $item) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_SECTION;
+  $menuitem['item']=$item;
+  array_push($menu, $menuitem);
 }
 
-function stc_menu() {
+function stc_menu_add_item (&$menu, $item, $url) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_ITEM;
+  $menuitem['item']=$item;
+  $menuitem['url']=$url;
+  array_push($menu, $menuitem);
+}
+
+function stc_menu_add_separator (&$menu) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_SEPARATOR;
+  array_push($menu, $menuitem);
+}
+
+function stc_menu_add_form (&$menu, $method, $action) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_FORM;
+  $menuitem['method']=$method;
+  $menuitem['action']=$action;
+  array_push($menu, $menuitem);
+}
+
+function stc_menu_form_add_text (&$menu, $label, $variable) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_FORM_TEXT;
+  $menuitem['label']=$label;
+  $menuitem['variable']=$variable;
+  array_push($menu, $menuitem);
+}
+
+function stc_menu_form_add_password (&$menu, $label, $variable) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_FORM_PASS;
+  $menuitem['label']=$label;
+  $menuitem['variable']=$variable;
+  array_push($menu, $menuitem);
+}
+
+function stc_menu_form_add_button (&$menu, $text) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_FORM_BUTTON;
+  $menuitem['text']=$text;
+  array_push($menu, $menuitem);
+}
+
+function stc_menu_form_end (&$menu) {
+  $menuitem = array();
+  $menuitem['type']=STC_MENU_FORM_END;
+  array_push($menu,$menuitem);
+}
+
+function stc_menu($menu) {
   ?><div id="main">
-<div id="menu"><?php
-    echo "menu\n";
-?></div>
-<div id="content">s
+<div id="menu">
+<?php
+  foreach ($menu as $menuitem) {
+    switch ($menuitem['type']) {
+    case STC_MENU_SECTION: echo "<div>".$menuitem['item']."</div>\n"; break;
+    case STC_MENU_ITEM: echo "<a href=\"".$menuitem['url']."\">".$menuitem['item']."</a>\n"; break;
+    case STC_MENU_SEPARATOR: echo "<hr/>\n";break;
+    case STC_MENU_FORM: echo "<form method=\"".$menuitem['method']."\" action=\"".$menuitem['action']."\">\n"; break;
+    case STC_MENU_FORM_TEXT: echo "<div><label for=\"".$menuitem['variable']."\">".$menuitem['label']."</label><input type=\"text\" name=\"".$menuitem['variable']."\"></input></div>\n"; break;
+    case STC_MENU_FORM_PASS: echo "<div><label for=\"".$menuitem['variable']."\">".$menuitem['label']."</label><input type=\"password\" name=\"".$menuitem['variable']."\"></input></div>\n"; break;
+    case STC_MENU_FORM_BUTTON: echo "<div><button>".$menuitem['text']."</button></div>\n"; break;
+    case STC_MENU_FORM_END: echo "</form>"; break;
+    }
+  }
+  ?></div><div id="menusepbar">&nbsp;</div>
+<div id="content">
 <?php
 }
 
@@ -36,5 +120,20 @@ function stc_footer() {
 </html>
 <?php
 }
+
+/******************************************************************************
+ *
+ * Gestion des utilisateurs
+ * 
+ */
+
+function stc_is_logged () {
+  return array_key_exists('userid',$_SESSION);
+}
+
+
+
+// gestion de la session
+session_start();
 
 ?>
