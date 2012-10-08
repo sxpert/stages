a223dcbcc8c506913c6fb62604d19dd9479776d0
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2012-10-08 16:59:39 +0200

    separation de la création de la base et du remplissage initial

--- sql/000-contents.sql
+++ sql/000-contents.sql
@@ -58,6 +58,11 @@ alter table type_offre add unique using index idx__type_offre__desc;
 grant usage on sequence seq__type_offre__id to stagesweb;
 grant select on type_offre to stagesweb;
 
+--insert into type_offre (code, description, denom_prop, denom_dir, has_year) values 
+--       ('TH', 'Thèse', 'Thèses', 'Directeur de Thèse', false);
+insert into type_offre (code, description, denom_prop, denom_dir, has_year) values 
+       ('MR', 'Master Recherche', 'Stages', 'Directeur de Stage', true);
+
 --
 -- sections cnrs
 -- 
@@ -153,6 +158,18 @@ $$ language plpgsql;
 
 create trigger trig_m2_set_from_value before insert on m2 for each row execute procedure m2_set_from_value ();
 
+insert into m2 (short_desc, description, ville) values ('A2P', 'Astrophysique, Plasmas et Planètes', 'Grenoble');
+insert into m2 (short_desc, description, ville) values ('Astro75', 'Astrophysique', 'Paris');
+insert into m2 (short_desc, description, ville) values ('DSG', 'Dynamique des Systèmes Gravitationnels', 'Paris');
+insert into m2 (short_desc, description, ville) values ('OSAE', 'Outils et Systèmes de l''Astronomie de de l''Espace', 'Paris');
+insert into m2 (short_desc, description, ville) values ('Astro67', 'Astrophysique UNISTRA', 'Strasbourg');
+insert into m2 (short_desc, description, ville) values ('ASEP', 'Astrophysique, Sciences de l''Espace, Planétologie', 'Toulouse');
+insert into m2 (short_desc, description, ville) values ('TSI', 'Techniques Spatiales et Instrumentation', 'Toulouse');
+insert into m2 (short_desc, description, ville) values ('OMEGA', 'Optique, Dynamique, Images, Astrophysique', 'Nice');
+insert into m2 (short_desc, description, ville) values ('AER', 'Astrophysique, Energie, Rayonnement', 'Marseille');
+insert into m2 (short_desc, description, ville) values ('APC', 'Astrophysique et Physique Corpusculaire', 'Bordeaux');
+insert into m2 (short_desc, description, ville) values ('PF', 'Physique Fondamentale', 'Lyon');
+
 --
 -- table des catégories
 -- (apparamment on peut avoir plusieurs catégories pour la meme offre)
@@ -175,6 +192,21 @@ grant select on categories to stagesweb;
 create view liste_categories as select id as key, description as value from categories;
 grant select on liste_categories to stagesweb;
 
+insert into categories(description) values 
+       ('Cosmologie, Univers primordial, origine et évolution des grandes structures de l''Univers et des galaxies'),
+       ('Astrophysique des hautes énergies, objets compacts, astroparticules, ondes gravitationnelles'),
+       ('Physique et chimie des milieux interstellaires et circumstellaires'),
+       ('Formation, structure et évolution des étoiles'),
+       ('Exoplanetes: origine, structure et évolution des systèmes planétaires, planétologie comparée'),
+       ('Système solaire: origine, composition, évolution, structure physico-chimique et dynamique de ses objets et des astromateriaux; cosmochimie'),
+       ('Physique du soleil et de l''héliosphere'),
+       ('Planétologie : physique, dynamique et chimie des atmosphères planétaires'),
+       ('Géophysique : Terre, atmosphère, ionosphère, magnétosphère'),
+       ('Processus physiques en astrophysique'),
+       ('Systèmes de référence spatio-temporels'),
+       ('Instrumentation pour les grands observatoires au sol et dans l''espace'),
+       ('Autres');
+
 --
 -- Statuts des utilisateurs
 --
@@ -189,6 +221,11 @@ grant select on statuts to stagesweb;
 
 create view liste_statuts as select id as key, description as value from statuts;
 grant select on liste_statuts to stagesweb;
+
+insert into statuts (description) values
+       ('Chercheur ou enseignant-chercheur en poste'),
+       ('Ingénieur de recherche'),
+       ('Chercheur en contrat post-doctoral');
 
 --
 -- Manager de projet
@@ -466,6 +503,14 @@ grant select on nature_stage to stagesweb;
 create view liste_nature_stage as select id as key, description as value from nature_stage;
 grant select on liste_nature_stage to stagesweb;
 
+insert into nature_stage values 
+       (1, 'Instrumentation'),
+       (2, 'Observations'),
+       (3, 'Simulations numériques'),
+       (4, 'Traitement de données'),
+       (5, 'Modélisation'),
+       (6, 'Expérimentation en laboratoire');
+
 --
 -- État de la demande de rémunération
 --
@@ -477,6 +522,10 @@ grant select on pay_states to stagesweb;
 
 create view liste_pay_states as select id as key, description as value from pay_states;
 grant select on liste_pay_states to stagesweb;
+
+insert into pay_states values
+       ( 1, 'Acquise'),
+       ( 2, 'En cours de négociation');
 
 --
 -- table des offres
