4b8bdb3254c3c46003014590d8c6d39cf44525a1
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2012-10-23 16:03:33 +0200

    fix file & directory access rights

--- account-access.php
+++ account-access.php
@@ -35,6 +35,7 @@ $options['access']=false;
 $options['home']=true;
 $menu = stc_default_menu($options);
 stc_menu($menu);
+echo "<h2>Probleme de mail de confirmation</h2>";
 echo "<p>Si vous n'avez pas reçu le mail de confirmation, remplissez le formulaire ci-dessous</p>\n";
 
 $form = stc_form("post", "account-access.php", $errors);
@@ -43,6 +44,14 @@ stc_form_password($form, "Mot de passe", "pass", $pass);
 stc_form_button($form, "Envoyer le mail de validation", "send_validation_email");
 stc_form_end();
 
+echo "<hr/>\n";
+echo "<h2>Changement de mot de passe</h2>\n";
+
+echo "<hr/>\n";
+echo "<h2>Perte de mot de passe</h2>\n";
+echo 
+
+echo "<hr/>\n
 echo "<p>Pour tout autre probleme, envoyez un mail à raphael [point] jacquot [@] obs [point] ujf-grenoble [point] fr</p>\n";
 
 
