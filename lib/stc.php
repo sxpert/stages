2dc37fac795f5d14a445cc0f1b6feafd75a4890b
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2012-11-27 14:27:04 +0100

    petite correction pour la mise en travaux du site de dev

--- lib/stc.php
+++ lib/stc.php
@@ -1422,6 +1422,20 @@ function stc_calc_year () {
 session_start();
 $db = stc_connect_db();
 
+if (($_SERVER['REMOTE_ADDR']!='193.107.127.8')&&
+    ($_SERVER['REMOTE_ADDR']!='127.0.0.1')&&
+    ($EN_TRAVAUX)) {
+  error_log("Travaux : ".$_SERVER['REMOTE_ADDR']);
+  stc_top();
+  $menu = stc_menu_init();
+  stc_menu_add_item($menu, "Accueil", "index.php");
+  stc_menu($menu);
+  echo "<h2>Site en travaux</h2>\n";
+  echo "<p>Le site revient bientôt, veuillez nous excuser pour le dérangement,</p>\n";
+  stc_footer();
+  exit(0);
+}
+
 $u = stc_user_id();
 if ($u!=0) {
   $sql = "select id from users_view where id=$1";
