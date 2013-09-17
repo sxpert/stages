eafab9a5de41c9be46ba7a111dc6994c496bd24b
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2013-09-17 11:45:09 +0200

    fix copyright statement

--- lib/stc.php
+++ lib/stc.php
@@ -836,7 +836,7 @@ function _append_scripts($scripts=null) {
 function stc_footer($scripts=null) {
   GLOBAL $_stc_scripts;
   stc_add_jquery ();
-  echo "</div></div>\n<div id=\"footer\">Conception Raphaël Jacquot 2011-2012<br/>\n";
+  echo "</div></div>\n<div id=\"footer\">Conception Raphaël Jacquot 2011-2013<br/>\n";
   echo "<a href=\"http://ipag.obs.ujf-grenoble.fr/?lang=fr\">";
   echo "<img src=\"/images/logo-ipag-small.png\"/>";
   echo "</a><br/>\n";
