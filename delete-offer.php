085abd87c1aedfb8e1e37a448c15b84f8560e498
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2013-09-17 16:28:22 +0200

    executes "deleting" of offers

--- delete-offer.php
+++ delete-offer.php
@@ -16,15 +16,22 @@ $super = ($admin===true);
 $offre_id = intval(stc_get_variable ($_REQUEST,'offreid'));
 $action = stc_get_variable ($_REQUEST, 'action');
 
-
-
 if (strcmp($action,"NO_DELETE")==0) {
   header("Location: /detail.php?offreid=".$offre_id);
   exit;
 }
 
+$errors=array();
 if (strcmp($action,"YES_DELETE")==0) {
-  
+
+	$sql = "update offres set deleted=true where id=$1 and id_project_mgr=$2;";
+	$res = pg_query_params($db, $sql, array($offre_id,$user));
+	$nb = pg_affected_rows($res);
+	if ($nb==1) {
+		header("Location: search.php?type=MR&projmgr=".$user);
+		exit;
+	}
+	stc_form_add_error($errors,'offreid','Vous n\'avez pas les droits pour supprimer cette entrée');
 }
 
 /*
@@ -36,7 +43,7 @@ $menu = stc_default_menu();
 stc_menu($menu);
 
 echo "<h2>Suppression d'une offre</h2>\n";
-$form = stc_form("POST", "delete-offer.php", null);
+$form = stc_form("POST", "delete-offer.php", $errors);
 echo "<input type=\"hidden\" name=\"offreid\" value=\"".$offre_id."\"/>";
 echo "<div>Êtes vous sûr(e) de vouloir supprimer l'offre :<br/>\n";
 
@@ -44,7 +51,7 @@ echo "<div>Êtes vous sûr(e) de vouloir supprimer l'offre :<br/>\n";
 
 
 echo "<div><button name=\"action\" value=\"NO_DELETE\">NON ce n'était pas<br/>\nce que je voulais faire</button>";
-echo " <button name=\"action\" values=\"YES_DELETE\">Oui, oui,<br/>\nje suis sûr(e) de moi</button></div>";
+echo " <button name=\"action\" value=\"YES_DELETE\">Oui, oui,<br/>\nje suis sûr(e) de moi</button></div>";
 echo "</div>\n";
 stc_form_end();
 
