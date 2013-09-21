4c122991238eb81c0d66265aed483555ddff3422
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2013-09-21 19:03:15 +0200

    add loggin features to tracck user actions

--- ajax/invalidate-offre.php
+++ ajax/invalidate-offre.php
@@ -39,13 +39,15 @@ if ($super||($admin==$m2_id)) {
 	$res = pg_query_params($db, $sql, array($offre_id, $m2_id));
 	$n = pg_affected_rows($res);
 	switch ($n) {
-		case 0: error('L\'offre n\'était pas validée pour cette M2R'); break;
-		case 1: ok (); break;
-		default:
+		case 0: stc_append_log('invalidate_offer','offer '.$offre_id.' was not valid for M2R '.$m2_id);
+			error('L\'offre n\'était pas validée pour cette M2R'); break;
+		case 1: stc_append_log('invalidate_offer','offer '.$offre_id.' was removed for M2R '.$m2_id-; ok (); break;
+		default: stc_append_log('invalidate_offer','case should not have happended, '.$n.' lines affected by query '.$offre_id.' '.$m2_id);
 			// should not happen
 	}
 	
 }
 
+stc_append_log('invalidate_offer','attempt to invalidate offer '.$offre_id.' from M2R '.$m2_id.' by unpriviledged user');
 error('Vous n\'avez pas le droit d\'effectuer cette opération');
 ?>
