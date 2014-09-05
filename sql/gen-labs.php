3e4f1cc859628535f601d5415d343d71d6008b09
Raphaël Jacquot <sxpert@elephant.sxpert.org>
2014-09-05 11:59:24 +0200

    nouveau génerateur de labs
    	* la page insu a été modifiée.

--- sql/gen-labs.php
+++ sql/gen-labs.php
@@ -8,10 +8,11 @@
  *
  ******************************************************************************/
 
-define ('SOURCE_URL', 'http://www.insu.cnrs.fr/co/unites/referentiel');
+define ('SOURCE_URL', 'http://www.insu.cnrs.fr/referentiel');
 
 /* loads the html file */
 $html = file_get_contents(SOURCE_URL, 'r');
+#echo $html;
 $doc = new DOMDocument ();
 $doc->loadHTML($html);
 
@@ -31,64 +32,69 @@ $table = $tables->item(0);
 $body = $table->getElementsByTagName('tbody');
 $lines = $body->item(0)->childNodes;
 $labos = array();
+
 foreach($lines as $line) {
-  if ($line->tagName=='tr') {
-    $l = array();
-    foreach($line->getElementsByTagName('td') as $item) {
-      $children = $item->childNodes;
-      if ($children->length>1) {
-	// find the last 'a' tag
-	foreach($children as $child)
-	  if (($child->nodeType==XML_ELEMENT_NODE)&&($child->tagName=='a'))
-	    $a = $child;
-	$attr = $a->attributes;
-	$url = $attr->getNamedItem('href');
-	$l['url'] = $url->textContent;
-	$children = $a->childNodes;
-	if($children->length!=1) {
-	  array_push($l,'unknown');
-	  continue;
+	if ($line->tagName=='tr') {
+		$l = array();
+		foreach($line->getElementsByTagName('td') as $item) {
+			$children = $item->childNodes;
+			if ($children->length>1) {
+				// find the last 'a' tag
+				foreach($children as $child)
+					if (($child->nodeType==XML_ELEMENT_NODE)&&($child->tagName=='a'))
+						$a = $child;
+				$attr = $a->attributes;
+				$url = $attr->getNamedItem('href');
+				$l['url'] = $url->textContent;
+			
+				$children = $a->childNodes;
+				if($children->length!=1) {
+					array_push($l,'unknown');
+					continue;
+				}
+		    }
+			array_push($l,trim($item->textContent));
+	    }
+    	array_push($labos, $l);
 	}
-      }
-      $child = $children->item(0);
-      if ($child->nodeType==XML_TEXT_NODE)
-	array_push($l,trim($child->textContent));
-    }
-    array_push($labos, $l);
-  }
 }
 
 $sectionsCNRS = array();
 
 foreach($labos as $id=>$labo) {
-  $l = array();
-
-  if (array_key_exists('url',$labo))
-    $l['url'] = $labo['url'];
-
-  $ln = trim($labo[0]);
-  $pos = strpos($ln,'(');
-  if ($pos==0) $pos = strlen($ln);
-  $l['name'] = trim(substr($ln,0,$pos));
-  $l['sigle'] = substr($ln,$pos+1,-1);
   
-  $ln = trim($labo[1]);
-  $l['typelabo'] = preg_replace('/^([A-Z]+)\d+$/','${1}',$ln);
-  $l['idlabo'] = preg_replace('/^[A-Z]+(\d+)$/','${1}',$ln);
+	$l = array();
+
+	if (array_key_exists('url',$labo))
+    	$l['url'] = $labo['url'];
+
+	$ln = trim($labo[0]);
+	$posi = strpos($ln,'(');
+	$poso = strrpos($ln,')');
+	$posm = strrpos($ln,'-');
+	if ($posi==0) {
+		if ($posm>0)
+			$posi = $posm;
+		else 
+			$posi = strlen($ln);
+	}
+	$l['name'] = trim(substr($ln,0,$posi));
+	$l['sigle'] = substr($ln,$posi+1,$poso-$posi-1);
+	$numlabo = trim(substr($ln, $posm+1));
+	$l['typelabo'] = preg_replace('/^([A-Z]+)\d+$/','${1}',$numlabo);
+	$l['idlabo'] = preg_replace('/^[A-Z]+(\d+)$/','${1}',$numlabo);
   
-  $l['ville'] = trim($labo[3]);
+	$l['ville'] = trim($labo[2]);
 
-  $ln = trim($labo[9]);
-  $pos = strpos($ln,':');
-  $sid = intval(substr($ln,0,$pos));
-  $snm = trim(substr($ln,$pos+1));
-  if ($sid!=0) {
-    if (!array_key_exists($sid,$sectionsCNRS))
-      $sectionsCNRS[$sid]=$snm;
-  } else $sid=null;
-  $l['section']=$sid;
+	$ln = trim($labo[8]);
+	$sid = intval($ln);
+	if ($sid!=0) {
+  		if (!array_key_exists($sid,$sectionsCNRS))
+			$sectionsCNRS[$sid]='';
+	} else $sid=null;
+	$l['section']=$sid;
 
-  $labos[$id] = $l;
+	$labos[$id] = $l;
 }
 
 // insertion dans la base de données
@@ -107,13 +113,13 @@ foreach($sectionsCNRS as $id => $description) {
 // ensuite, les labos
 $sql = "insert into laboratoires (type_unite, id, id_section, sigle, description, city) values ($1,$2,$3,$4,$5,$6);";
 foreach($labos as $labo) {
-  pg_query_params($db, $sql, 
-		  array ($labo['typelabo'], 
-			 $labo['idlabo'],
-			 $labo['section'],
-			 $labo['sigle'],
-			 $labo['name'],
-			 $labo['ville']));
+	pg_query_params($db, $sql, 
+		array ($labo['typelabo'], 
+			$labo['idlabo'],
+			$labo['section'],
+			$labo['sigle'],
+			$labo['name'],
+			$labo['ville']));
 }
 
 
