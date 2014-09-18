ff8e0d6d11065f10ca4bc432bc8dfbe8467d617e
Raphaël Jacquot <sxpert@elephant.sxpert.org>
2014-09-18 18:15:14 +0200

    database creation update
    	* start a new script for creation of the database
    	  will allow for automatic upgrade
    	* edit more iso-3166-1 data

---
+++ sql/build-db.sql
@@ -0,0 +1,31 @@
+do $$
+	begin
+		--
+		-- upgrade 1 :
+		-- table des pays
+		-- 
+		perform table_name from information_schema.tables where table_schema='public' and table_type='BASE TABLE' and table_name='coutries';
+		if not found then
+			-- create the table
+			create table countries (
+				iso2	char(2),
+				name	text
+			); 
+			create unique index pk__countries__iso2 on countries ( iso2 );
+			alter table countries add primary key using index pk__countries__iso2;
+			
+			grant select on countries to stagesweb;
+
+			-- insert some countries
+		
+			insert into countries ( iso2, names ) values
+				('CH', 'Suisse' ),
+				('CL', 'Chili' ),
+				('DE', 'Allemagne' ),
+				('ES', 'Espagne' ),
+				('FR', 'France' ),
+				('GB', 'Grande Bretagne' ),
+				('IT', 'Italie' );
+		end if;
+	end;
+$$;
