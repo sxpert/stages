267d2e5eda092fc279e92460fd85c063a6d66018
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2019-08-20 11:46:38 +0200

    add creating a new configuration variable from

--- detail-config.php
+++ detail-config.php
@@ -30,61 +30,103 @@ if (array_key_exists ('action', $_REQUEST))
 
 // get info on the variable
 $key = stc_get_variable($_REQUEST, 'key');
-$sql = "select * from config_vars where key=$1;";
-$r = pg_query_params($db, $sql, array($key));
-$n = pg_num_rows($r);
-if ($n != 1) {
-	// not a valid configuration variable
-	top();
-	echo "<p>Impossible de trouver la variable de configuration '".$key."'</p>";
-	// not an integer
-	stc_footer();
-	exit (0);
-}
-$varinfo = pg_fetch_assoc($r);
-$var_type = $varinfo['var_type'];
-pg_free_result($r);
 
-// find the data about this variable.
-// if not found, add the variable
-$sql = "select * from config where key=$1 order by version_date desc;";
-$r = pg_query_params($db, $sql, array($key));
-$n = pg_num_rows($r);
-if (!isset($action)&&($n == 0)) {
-	// can't find any record for this variable
-	$action = 'new-config';
-}
+if (!isset($action)) { 
+	$sql = "select * from config_vars where key=$1;";
+	$r = pg_query_params($db, $sql, array($key));
+	$n = pg_num_rows($r);
+	if ($n != 1) {
+		// not a valid configuration variable
+		top();
+		echo "<p>Impossible de trouver la variable de configuration '".$key."'</p>";
+		// not an integer
+		stc_footer();
+		exit (0);
+	}
+	$varinfo = pg_fetch_assoc($r);
+	$var_type = $varinfo['var_type'];
+	pg_free_result($r);
 
+	// find the data about this variable.
+	// if not found, add the variable
+	$sql = "select * from config where key=$1 order by version_date desc;";
+	$r = pg_query_params($db, $sql, array($key));
+	$n = pg_num_rows($r);
+	if (!isset($action)&&($n == 0)) {
+		// can't find any record for this variable
+		$action = 'new-config';
+	}
+}
 
 if (isset($action)) {
 	
 	$errors = array();
 
+	$description = stc_get_variable($_POST, "description");
+	$type = stc_get_variable($_POST, "type");
 	$value = stc_get_variable($_POST, 'value');
 	
+	$drop = FALSE;
+
 	switch ($action) {
+	case "create-new-config":
+		error_log("create-new-config: got data '".$key."', '".$description."', '".$type."'");
+		$sql = "insert into config_vars (key, description, var_type) values ($1, $2, $3);";
+		$dba = db_connect_adm();
+		pg_query($dba, "begin;");
+		$drop = TRUE;
+		$val = array($key, $description, $type);
+		pg_send_query_params($dba, $sql, $val);
+		$res = pg_get_result($dba);
+		$err = pg_result_error_field($res, PGSQL_DIAG_SQLSTATE);
+		if (!is_null($err)) {
+			switch ($err) {
+				case '23505':
+					stc_form_add_error($errors, 'key', 'une variable existe déjà avec ce nom');
+					break;
+				case '22P02':
+					stc_form_add_error($errors, 'type', 'invalid type not in enum');
+					break;
+				default:
+					$errmsg = pg_result_error($res);
+					error_log("PG_DIAG_SQL_STATE :".$err." - ".$errmsg);
+			}
+		} 
+		# if we have errors, break here
+		if (count($errors)>0) 
+			break;
+		# fall down to create the first entry
+		error_log("create-new-config: fall down to create new value");
 	case "create-config":
+		error_log("create-config: got key value '".$key."', '".$value."'");
 		$sql = "insert into config (key, value, version_date) values ($1, $2, now());";
-		$dba = db_connect_adm ();
+		# create a new connection only if we're not dropping
+		if (!$drop)
+			$dba = db_connect_adm ();
 		$val = array($key, $value);
 		pg_send_query_params ($dba, $sql, $val);
 		$res = pg_get_result ($dba);
 		$err = pg_result_error_field ($res, PGSQL_DIAG_SQLSTATE);
 		if (!is_null($err)) {
 			if ($err=='23505') {
-				stc_form_add_error ($errors, 'short_desc', 'un m2 existe déja');
+				stc_form_add_error ($errors, 'key', 'une clé/valeur identique existe déja');
 			} else {
-				error_log ("PGSQL_DIAG_SQLSTATE :".$err);
+				$errmsg = pg_result_error($res);
+				error_log ("PGSQL_DIAG_SQLSTATE :".$err." - ".$errmsg);
+				// output some error message
 			}
 		} else {
+			if ($drop)
+				pg_query($dba, "commit;");
 			header("Location: liste-configs.php");
 			exit(0);
 		}
+		break;
 	case "new-config":
 		# stuff for form
-		$caption = "Ajout d'une nouvelle variable '".$key."' (".$var_type.")";
+		$caption = "Ajout d'une nouvelle variable";
 		$new_button = "Créer la variable";
-		$new_action = "create-config";
+		$new_action = "create-new-config";
 		break;
 	case "edit-config":
 		$row = pg_fetch_assoc($r);
@@ -100,12 +142,20 @@ if (isset($action)) {
 	top ();
 	echo "<h2>".$caption."</h2>\n";
 	$form = stc_form ('post', 'detail-config.php', $errors);
+	if ($action=="new-config") {
+		stc_form_text($form, "Nom", "key", $key);
+		stc_form_text($form, "Description", 'description', $description);
+		# get the values for the enum	e
+		#stc_form_text($form, "Type", "type", $type);
+		stc_form_select ($form, "Type", "type", $type, "liste_config_var_type" ); 
+	} else {
+		stc_form_hidden($form, "key", $key);
+	}
 	switch($var_type) {
 	default:
 		stc_form_text ($form, "Valeur", "value", $value);
 		break;
 	}
-	stc_form_hidden($form, "key", $key);
 	stc_form_button ($form, $new_button, $new_action);
 	stc_form_end ();
 
