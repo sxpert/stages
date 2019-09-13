a31f372db61fa965a8e6415f7a50d9b584639101
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2019-09-13 15:26:31 +0200

    envoi de mails en cas de compte bloqué

--- login.php
+++ login.php
@@ -31,11 +31,27 @@ if (strcmp($_SERVER['REQUEST_METHOD'],'POST')==0) {
   case -1: 
     $_SESSION['loginerr']="Compte bloqué"; 
     # envoi de mails
+    $admin_mail = stc_config_get ('ADMIN_EMAIL', $default=null);
     $dba = db_connect_adm();
     $res = pg_query_params($dba, "select f_name, l_name, email from users where login=$1;", array($login));
     $row = pg_fetch_assoc($res);
     pg_close($dba);
+    $f_name = $row['f_name'];
+    $l_name = $row['l_name'];
+    $email = $row['email'];
     error_log("ACCOUNT LOCKED ".print_r($row));
+    error_log("sending mail to ".$f_name.' '.$l_name.' <'.$email.'>');
+    $subject = "[Stages Masters M2] Votre compte est bloqué";
+    $message  = "Bonjour ".$f_name.' '.$l_name.",\r\n\r\n";
+    $message .= "Votre compte '".$login."' est bloqué suite a de multiples tentatives de connections infructueuses\r\n\r\n";
+    $message .= "Si vous n'êtes pas a l'origine de ces connections, contactez nous à ".$admin_mail.".\r\n"; 
+    error_log($subject);
+    error_log($message);
+    stc_send_email ($email, $subject, $message);
+
+    $subject = "Le compte ".$login." est bloqué";
+    $message = "Mail de contact : ".$email."\r\n\r\nMerci de le débloquer.\r\n";
+    stc_send_email ($admin_mail, $subject, $message);
 
     break;
   case  0: 
